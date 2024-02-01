9ce21185df5608eda54ed329cd9bdf739a84abc3
EvertonCarvalho1 <everton.carvalho.dc@gmail.com>
2024-02-01 13:08:37 -0300

    react native testing library

--- src/components/StarRating/__tests__/StarRating.test.tsx
+++ src/components/StarRating/__tests__/StarRating.test.tsx
@@ -2,7 +2,14 @@ import React from 'react';
 import { render } from '@testing-library/react-native';
 import { StarRating } from '../StarRating';
 
-test('the component rendered', () => {
-    //ecrever test
-    render(<StarRating rating={{ average: 7 }} />);
-}); 
+describe('StarRating', () => {
+    test('if passed rating show the average', () => {
+        //ecrever test
+        const { getByText, getByTestId } = render(<StarRating rating={{ average: 7 }} />);
+
+        const element = getByText('7');
+        const icon = getByTestId('starIcon');
+
+        expect(element).toBeTruthy();
+    });
+});
