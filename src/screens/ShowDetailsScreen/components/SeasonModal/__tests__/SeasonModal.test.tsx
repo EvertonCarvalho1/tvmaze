d8728a39c44bc5a879f9f65c8bea9ad76b71d798
EvertonCarvalho1 <everton.carvalho.dc@gmail.com>
2024-02-08 12:36:28 -0300

    react native testing library

--- src/screens/ShowDetailsScreen/components/SeasonModal/__tests__/SeasonModal.test.tsx
+++ src/screens/ShowDetailsScreen/components/SeasonModal/__tests__/SeasonModal.test.tsx
@@ -1,16 +1,21 @@
-import React from 'react';
+import React, { createRef } from 'react';
 import { render } from '@testing-library/react-native'
 import { SeasonModal } from '../SeasonModal';
+import { Modalize } from 'react-native-modalize';
 
 
 describe('SeasonModal', () => {
     test('the component rendered', () => {
-        render(
+        const modalizeRef = createRef<Modalize>();
+
+        const { debug } = render(
             <SeasonModal
                 seasons={['1', '2', '3']}
                 selectedSeason='1'
                 onSelectSeason={(season) => console.log(season)}
             />
-        )
+        );
+
+        debug();
     })
 })
